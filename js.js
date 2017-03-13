06364ba85c817cbacaf10620203353330781f2bf
organizzzm <organizzzm@gmail.com>
2017-03-13 13:07:02 +0200

    little fix

--- js.js
+++ js.js
@@ -1,71 +1,79 @@
-$(function () {
+var JustifyGallery = function () {
+    this.HEIGHTS = [];
+    this.list = [];
+    this.$container = $('.gallery');
+};
 
-    HEIGHTS = [];
-
-    function getheight(images, width) {
-        width -= images.length * 5;
-        var h = 0;
-        for (var i = 0; i < images.length; ++i) {
-            h += $(images[i]).data('width') / $(images[i]).data('height');
-        }
-        return width / h;
+JustifyGallery.prototype.getHeight = function (data, width) {
+    width -= data.length * 5;
+    var h = 0;
+    for (var i = 0; i < data.length; ++i) {
+        h += data[i].width / data[i].height;
     }
+    return width / h;
+};
 
-    function setheight(images, height) {
-        HEIGHTS.push(height);
-        for (var i = 0; i < images.length; ++i) {
-            $(images[i]).css({
-                width: height * $(images[i]).data('width') / $(images[i]).data('height'),
-                height: height
-            });
-            $(images[i]).attr('src', $(images[i]).attr('src').replace(/w[0-9]+-h[0-9]+/, 'w' + $(images[i]).width() + '-h' + $(images[i]).height()));
-        }
-    }
+JustifyGallery.prototype.setHeight = function (data, height) {
+    this.HEIGHTS.push(height);
 
-    function resize(images, width) {
-        setheight(images, getheight(images, width));
-    }
+    for (var i = 0; i < data.length; ++i) {
+        var width = height * data[i].width / data[i].height,
+            height = height;
 
-    function run(max_height) {
-        var size = window.innerWidth - 17;
+        var container = $('<div class="item"><img></div>');
+        var img = container.find('img');
 
-        var n = 0;
-        var images = $('img');
-        w: while (images.length > 0) {
-            for (var i = 1; i < images.length + 1; ++i) {
-                var slice = images.slice(0, i);
-                var h = getheight(slice, size);
-                if (h < max_height) {
-                    setheight(slice, h);
-                    n++;
-                    images = images.slice(i);
-                    continue w;
-                }
-            }
-            setheight(slice, Math.min(max_height, h));
-            n++;
-            break;
-        }
-        console.log(n, slice);
-    }
-
-    window.addEventListener('resize', function () {
-        run(200);
-    });
-
-    $.getJSON('http://82.196.1.83:9571/', function (data) {
-        const container = document.querySelector('.gallery');
-        let html = '';
-
-        data.forEach((item) => {
-            html += `<img src="http://82.196.1.83:9571/${item.filename}" data-width="${item.width}" data-height="${item.height}">`
+        container.css({
+            width: width,
+            height: height
         });
 
-        container.innerHTML = html;
-        run(200);
+        img.attr('src', `http://82.196.1.83:9571/${data[i].filename}`);
 
+        img.on('error', function(e){
+            // e.target.parentNode.remove();
+        });
+
+        img.on('load', function (e) {
+            e.target.classList.add('show');
+        });
+
+
+        this.list.push(container);
+    }
+};
+
+JustifyGallery.prototype.render = function (data, maxHeight, size) {
+    var size = size,
+        n = 0, list = data;
+    this.list = [];
+    this.HEIGHTS = [];
+
+    w: while (list.length > 0) {
+        for (var i = 1; i < list.length + 1; i++) {
+            var slice = list.slice(0, i);
+            var h = this.getHeight(slice, size);
+            if (h < maxHeight) {
+                this.setHeight(slice, h);
+                n++;
+                list = list.slice(i);
+                continue w;
+            }
+        }
+        this.setHeight(slice, Math.min(maxHeight, h));
+        n++;
+        break;
+    }
+
+    this.$container.html(this.list);
+};
+
+var justifyGallery = new JustifyGallery();
+
+$.getJSON('http://82.196.1.83:9571/', function (data) {
+    justifyGallery.render(data, 200, $('.gallery').innerWidth() - 17);
+
+    window.addEventListener('resize', function () {
+        justifyGallery.render(data, 200, $('.gallery').innerWidth());
     });
-
-
 });
-
